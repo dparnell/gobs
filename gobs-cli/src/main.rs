563e9a6f4414929d6cbca8a767116af1858befdf
Daniel Parnell <me@danielparnell.com>
2020-04-13 14:55:46 +1000

    added code to export vox to json

--- gobs-cli/src/main.rs
+++ gobs-cli/src/main.rs
@@ -1,22 +1,88 @@
+extern crate structopt;
+extern crate dot_vox;
+
+use structopt::{StructOpt};
+
 use gobs_core::raw_volume::RawVolume;
 use gobs_core::region::Region;
 use gobs_core::volume::Volume;
 use gobs_core::raw_volume_sampler::RawVolumeSampler;
 use gobs_core::cubic_surface_extractor::extract_cubic_mesh;
+use dot_vox::load;
+use std::fs::File;
+use std::io::{self, Error, Write};
 
-fn main() {
-    let region = Region::cubic(16);
-    let mut volume: RawVolume<i32> = RawVolume::new(region);
-    volume.set_voxel_at(8, 8, 8, 1).unwrap();
-    //volume.set_voxel_at(9, 8, 8, 1).unwrap();
-    //volume.set_voxel_at(10, 8, 8, 1).unwrap();
+#[derive(StructOpt, Debug)]
+#[structopt(name = "gobs-cli")]
+struct Options {
+    #[structopt(name = "vox-file")]
+    vox_file: String,
 
-    let mut sampler = RawVolumeSampler::new(&volume);
-    let mesh = extract_cubic_mesh(&mut sampler, &Region::cubic(16)).unwrap();
+    #[structopt(name = "out-file")]
+    output: Option<String>
+}
 
-    let vertex_count = mesh.vertices.len();
-    let index_count = mesh.indices.len();
+impl Options {
+    fn get_output(&self) -> Result<Box<dyn Write>, Error> {
+        match self.output {
+            Some(ref path) => File::create(path).map(|f| Box::new(f) as Box<dyn Write>),
+            None => Ok(Box::new(io::stdout())),
+        }
+    }
+}
 
-    println!("vertex count = {}, index count = {}", vertex_count, index_count);
-    println!("{:?}", mesh.vertices);
+fn main() -> std::io::Result<()> {
+    let options = Options::from_args();
+    let mut out = options.get_output().unwrap();
+
+    let vox_file = load(&options.vox_file).unwrap();
+
+    let hex_palette = vox_file.palette.iter().map(|rgba| {
+        let a = (rgba >> 24) as u8;
+        let b = (rgba >> 16) as u8;
+        let g = (rgba >> 8) as u8;
+        let r = *rgba as u8;
+
+        format!("{{\"r\":{},\"g\":{},\"b\":{},\"a\":{}}}", r, g, b, a)
+    } ).collect::<Vec<String>>().join(", ");
+
+    writeln!(out, "{{")?;
+    writeln!(out, " \"palette\": [{}],", hex_palette)?;
+    writeln!(out, " \"models\": [")?;
+    let mut first = true;
+    for model in vox_file.models {
+        let region = Region::sized(model.size.x as i32, model.size.y as i32, model.size.z as i32);
+        let mut volume= RawVolume::new(region);
+
+        for voxel in model.voxels {
+            volume.set_voxel_at(voxel.x as i32, voxel.y as i32, voxel.z as i32, voxel.i).unwrap();
+        }
+
+        let mut sampler = RawVolumeSampler::new(&volume);
+        let mesh = extract_cubic_mesh(&mut sampler, &volume.valid_region).unwrap();
+
+        let vertices = mesh.vertices.iter()
+            .map(|v| {
+                let pos = v.decode();
+                format!("{{\"x\":{},\"y\":{},\"z\":{},\"c\":{}}}", pos.x, pos.y, pos.z, v.data)
+            })
+            .collect::<Vec<String>>().join(",");
+
+        let polygons = mesh.indices.chunks(3).map(|tri| format!("[{},{},{}]", tri[0], tri[1], tri[2]))
+            .collect::<Vec<String>>().join(",");
+
+        if first {
+            first = false;
+        } else {
+            writeln!(out, ",")?;
+        }
+        writeln!(out, "  {{")?;
+        writeln!(out, "   \"vertices\": [{}],", vertices)?;
+        writeln!(out, "   \"polygons\": [{}]", polygons)?;
+        write!(out, "  }}")?;
+    }
+    writeln!(out, "\n ]")?;
+    writeln!(out, "}}")?;
+
+    Ok(())
 }
